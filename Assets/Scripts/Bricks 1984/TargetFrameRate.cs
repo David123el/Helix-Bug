612e7a477934072c416f80607ba4a014e47d5aa6
Noam Guterman <noamguterman@gmail.com>
2018-11-26 17:28:46 +0200

    no message

---
+++ Assets/Scripts/Bricks 1984/TargetFrameRate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFrameRate : MonoBehaviour {
+
+    public static int targetFrameRate;
+
+    // Use this for initialization
+    void Start () {
+
+        Application.targetFrameRate = 60;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
